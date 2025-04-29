47e963d957b2edd8d8d3b9af4364ae7b3d629896
svoren258 <svoren258@gmail.com>
2025-04-29 10:40:59 +0200

    fix(backend): hello world

--- react/libs/transactions-data-access/src/lib/get-balance.ts
+++ react/libs/transactions-data-access/src/lib/get-balance.ts
@@ -1,4 +1,5 @@
 export function getBalance(): Promise<number> {
+  console.log('hello world');
   return fetch('http://localhost:5087/balance')
     .then((response) => response.json() as Promise<{ amount: number }>)
     .then((data) => data.amount);
